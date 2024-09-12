068e1ee4f5c498f8c530805666afd2e088d461f1
felipscarvalho <felipecarvalho5520@gmail.com>
2024-09-12 17:46:04 -0300

    Team members name added

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /**
+ * Felipe Carvalho Leal
+ * Isaías Cruz Lessa
+ * Luan Amaro dos Santos Aragão
+ */
+
+/**
  * Dada uma lista com 5 elementos, [100, 80, 60, 120, 90], devolva a soma de todos os elementos, só que usando Map no resultado (O que seria usar map no resultado?)
  * Todas as funções de alta ordem vistas em sala de aula estão liberadas para uso
  * Meio ponto extra para os membros das equipes
